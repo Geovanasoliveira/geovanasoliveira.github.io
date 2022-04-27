2da4487703e4b44b5f53098319e60732b88a028e
Geovana Cristina da Silva Oliveira <g.a.silva.oliveira@avanade.com>
2022-04-27 15:14:50 -0300

    Aula3

---
+++ assets/js/todo.js
@@ -0,0 +1,12 @@
+console.log("oie");
+
+const nome = 'Geovana Oliveira';
+let email = 'geovana@outlook.com';
+console.log(`Nome: ${nome} Email: ${email}`);
+
+function escreve(x) { 
+    console.log("From: " + x);
+}
+
+escreve(`Seja bem-vindo ${nome}`);
+escreve("Treinamento HTML5, CSS3 e JS");
